b0594e644419dfe94c7ecb26827ed66a15510dcc
Rodolpho Reis <rodolphoreis@live.com>
2024-06-28 15:31:37 +0100

    feat: add Nav component

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,10 @@ export default function RootLayout({ children }) {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );
