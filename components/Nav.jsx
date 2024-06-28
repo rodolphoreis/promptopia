1bffdab39c633319cdb9415cb002700945de0907
Rodolpho Reis <rodolphoreis@live.com>
2024-06-28 16:29:05 +0100

    feat: create const isUserLoggedIn

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
