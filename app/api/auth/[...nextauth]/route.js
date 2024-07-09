1b07068f56ac6678f40a6f9802eb995508906d78
Rodolpho Reis <rodolphoreis@live.com>
2024-07-09 18:33:08 +0100

    feat(auth): add session handling and sign-in logic
    Implemented session handling to attach user ID to session object. Added sign-in logic to connect to the database, check for existing users, and create new users if they do not exist.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "@models/user";
+import { connectToDB } from "@/utils/database";
+
 const handle = NextAuth({
   providers: [
     GoogleProvider({
@@ -8,8 +11,40 @@ const handle = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      //check if user already exists
+      const userExists = await User.findOne({ email: profile.email });
+
+      // if not, create new user
+      if (!userExists) {
+        const newUser = await User.create({
+          email: profile.email,
+          userName: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.image,
+        });
+
+        return newUser;
+      }
+      return newUser;
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handle as GET, handle as POST };
